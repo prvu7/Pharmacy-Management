6bb93384d6f315f2ef5f625e81cd83d8bbb069d6
Popa Marian-Iulian <popaiulian253@gmail.com>
2025-11-23 13:16:50 +0200

    Added database connection
    Configured database connection in application.yaml.
    Added .env-example for tracking .env variables.
    Modify .env values to match your systems.

--- src/main/java/com/mpp/pharmacy/PharmacyApplication.java
+++ src/main/java/com/mpp/pharmacy/PharmacyApplication.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 // ONLY TESTING RIGHT NOW THIS REMOVES DATABASE CONNCECTION ERRORS
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class PharmacyApplication {
 
 	public static void main(String[] args) {
